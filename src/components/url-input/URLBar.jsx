967ce3fca10512c548e67cfe6d373a3940bc6a11
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-02 23:17:50 +0000

    Added state to the URL input component

--- src/components/url-input/URLBar.jsx
+++ src/components/url-input/URLBar.jsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function URLBar() {
+  const [inputURL, setInputURL] = useState("");
+
+  const submitURL = (e) => {
+    e.preventDefault();
+    console.log(inputURL);
+  };
+
   return (
     <section id="url-section">
-      <form className="p-10">
+      <form className="pt-10" onSubmit={(e) => submitURL(e)}>
         <label htmlFor="url-input" />
         <p>Enter URL</p>
-        <input type="text" className="rounded-md w-4/5" required />
+        <input
+          type="text"
+          className="rounded-md w-4/5"
+          required
+          onChange={(e) => setInputURL(e.target.value)}
+        />
+        <button type="submit" className="block m-auto mt-6 p-1 bg-white w-1/4">
+          Submit
+        </button>
       </form>
     </section>
   );
