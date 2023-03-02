64fdf715592eb05b149bbcfef5ceaa92adef1854
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-02 23:21:33 +0000

    HomePage style changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import URLBar from "../components/url-input/URLBar";
 export default function HomePage() {
   return (
     <section id="home-page">
-      <h2>Home page</h2>
+      <h2 className="text-white font-bold underline">Home page</h2>
       <URLBar />
     </section>
   );
