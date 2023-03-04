b11c83c9bfc59f28bb7c2c2a74b4e946619f2d4e
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-04 21:47:44 +0000

    Added parametric endpoint for URL redirect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import RedirectToMainURL from "./components/RedirectToMainURL";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App bg-gray-600 p-6 h-screen">
+      <Routes>
+        <Route path="/:shortened_url" element={<RedirectToMainURL />} />
+      </Routes>
       <h1 className="text-white font-bold">URL Shortener</h1>
       <HomePage />
     </div>
