524b97418d5963830a74e98c6087adfebb45e123
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-04 16:28:33 +0000

    Added width to root element to cover entire screen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App bg-gray-600 p-6">
+    <div className="App bg-gray-600 p-6 h-screen">
       <h1 className="text-white font-bold">URL Shortener</h1>
       <HomePage />
     </div>
