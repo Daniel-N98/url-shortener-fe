db3c25c2c6e70a7009dfe7f82ba4b62d3878875c
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-04 16:14:02 +0000

    Added base URL output section

---
+++ src/components/url-input/URLOutput.jsx
@@ -0,0 +1,9 @@
+export default function URLOutput({ SHORT_URL }) {
+  return (
+    <section id="url-output-section">
+      <p>
+        Here's your shortened URL <i>https://shorturl.com/{SHORT_URL}</i>
+      </p>
+    </section>
+  );
+}
