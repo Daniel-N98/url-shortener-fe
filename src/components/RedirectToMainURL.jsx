65958908e7538aad58ebcc7f2ad362c6e0d4ff08
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-04 21:48:22 +0000

    Base redirect component

---
+++ src/components/RedirectToMainURL.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BASE_URL } from "./url-input/URLBar";
+
+export default function RedirectToMainURL() {
+  const { shortened_url } = useParams();
+
+  async function fetchURL() {
+    try {
+      const { data } = await BASE_URL.get(`/url/:${shortened_url}`);
+      window.location.href = data.URL;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    fetchURL();
+  });
+
+  return null;
+}
