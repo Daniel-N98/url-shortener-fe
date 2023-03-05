58280ac954dd1b0ccd0720206da5da11a2ed77a4
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-05 18:33:56 +0000

    Added base styling for the URLOutput component

--- src/components/url-input/URLOutput.jsx
+++ src/components/url-input/URLOutput.jsx
@@ -1,10 +1,19 @@
+import "./urlOutput.css";
+
 export default function URLOutput({ SHORT_URL }) {
   const URL = `https://incomparable-seahorse-f9d25e.netlify.app/${SHORT_URL}`;
   return (
-    <section id="url-output-section">
-      <p>
-        Here's your shortened URL <a href={URL}>{URL}</a>
-      </p>
+    <section
+      id="url-output-section"
+      className="bg-green-500 w-auto m-auto rounded-md p-3 mt-6 text-green-900 underline"
+    >
+      <p className="font-bold mb-4">Success, shortened URL below</p>
+      <div
+        id="url-container"
+        className="bg-slate-200 p-1 pt-2 pb-2 rounded-md text-blue-700 underline text-xs"
+      >
+        <a href={URL}>{URL}</a>
+      </div>
     </section>
   );
 }
