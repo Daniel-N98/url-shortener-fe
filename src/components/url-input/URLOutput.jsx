4e747322ec7c1cc1be323f55bbfb91b369225b6c
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-05 16:47:55 +0000

    Updated output link to the actual link

--- src/components/url-input/URLOutput.jsx
+++ src/components/url-input/URLOutput.jsx
@@ -1,8 +1,9 @@
 export default function URLOutput({ SHORT_URL }) {
+  const URL = `https://incomparable-seahorse-f9d25e.netlify.app/${SHORT_URL}`;
   return (
     <section id="url-output-section">
       <p>
-        Here's your shortened URL <i>https://shorturl.com/{SHORT_URL}</i>
+        Here's your shortened URL <a href={URL}>{URL}</a>
       </p>
     </section>
   );
