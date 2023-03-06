47e9edb84db0978636f7b69484fddfcf0848615b
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-06 20:28:36 +0000

    Added routes and navigation to app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import NavBar from "./components/navigation/Navbar";
 import RedirectToMainURL from "./components/RedirectToMainURL";
+import AccountPage from "./pages/AccountPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App bg-gray-600 p-6 h-screen">
+      <h1 className="text-white font-bold">URL Shortener</h1>
+      <NavBar />
       <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/account" element={<AccountPage />} />
         <Route path="/:shortened_url" element={<RedirectToMainURL />} />
       </Routes>
-      <h1 className="text-white font-bold">URL Shortener</h1>
-      <HomePage />
     </div>
   );
 }
