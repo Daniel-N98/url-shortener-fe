4b61bd148b94c160e76207c4ac8a5be853cc350d
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-06 20:54:28 +0000

    Added URLBar to home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,9 @@
+import URLBar from "../components/url-input/URLBar";
+
 export default function HomePage() {
-  return <section id="home-page"></section>;
+  return (
+    <section id="home-page">
+      <URLBar />
+    </section>
+  );
 }
