1f81023c1c8aa7b6be9d81f9057bb974f298da7e
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-08 22:32:37 +0000

    Removed const

--- src/firebase/firebase.auth.js
+++ src/firebase/firebase.auth.js
@@ -7,7 +7,6 @@ import { auth } from "./firebase";
 export async function registerUser({ EMAIL, PASSWORD }, setErrors) {
   return createUserWithEmailAndPassword(auth, EMAIL, PASSWORD)
     .then(() => {
-      const user = auth.currentUser;
       return true;
     })
     .catch((error) => {
