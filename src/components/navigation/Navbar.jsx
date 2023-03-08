5ae448bcd9ffbc6b67d485fccf219f27f7f3d7fc
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-08 21:55:20 +0000

    Navbar style

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
+  const LINK_CLASSES =
+    "text-blue-300 font-bold hover:underline hover:text-white active:text-slate-300";
   return (
     <section id="nav-bar-section">
       <nav>
-        <ul className="w-1/3 m-auto mb-20">
-          <Link
-            to="/home"
-            className="text-blue-300 font-bold m-3 underline hover:text-white active:text-slate-300"
-          >
+        <ul className="w-2/3 m-auto mb-20 flex justify-center gap-6">
+          <Link to="/home" className={LINK_CLASSES}>
             Home
           </Link>
-          <Link to="/account" className="text-blue-300 font-bold m-3">
+          <Link to="/account" className={LINK_CLASSES}>
             Account
           </Link>
         </ul>
