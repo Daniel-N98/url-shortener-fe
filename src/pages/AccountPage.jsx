c21c027fe1c1a5ddfccd453e4230572bce45483e
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-09 20:05:39 +0000

    Updated button style

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -8,7 +8,7 @@ export default function AccountPage() {
   if (loading) return <h2>Loading...</h2>;
 
   const handleSignOut = async () => {
-    await signOut();
+    await signOut(auth);
   };
 
   return (
@@ -17,7 +17,7 @@ export default function AccountPage() {
         <section id="account">
           <p>Signed in</p>
           <button
-            className="block m-auto rounded-md w-4/5 p-1 mt-2 mb-2 text-center"
+            className="inline h-9 w-36 m-auto mt-3 p-1 bg-white rounded-md font-bold border-black border-2 hover:bg-slate-200 active:bg-slate-400 active:text-white transition-all"
             onClick={() => handleSignOut()}
           >
             Sign out
