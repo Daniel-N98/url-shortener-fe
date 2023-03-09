a1efd8c5059a85597cff4a5b3695cf69b8b737ba
Daniel-N98 <98589475+Daniel-N98@users.noreply.github.com>
2023-03-09 20:03:40 +0000

    Added signout button

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,15 +1,28 @@
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 import LoginForm from "../components/user/login/LoginForm";
+import { signOut } from "firebase/auth";
 
 export default function AccountPage() {
   const [user, loading] = useAuthState(auth);
   if (loading) return <h2>Loading...</h2>;
 
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
   return (
     <section id="account-page">
       {user ? (
-        <section id="account">Signed in</section>
+        <section id="account">
+          <p>Signed in</p>
+          <button
+            className="block m-auto rounded-md w-4/5 p-1 mt-2 mb-2 text-center"
+            onClick={() => handleSignOut()}
+          >
+            Sign out
+          </button>
+        </section>
       ) : (
         <section id="register-login-buttons">
           <LoginForm />
